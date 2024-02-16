33e4ddaea014149d70d42e44b76e96b821080f3f
25957848 <25957848@sun.ac.za>
2024-02-16 10:22:28 +0200

    started multithreading

--- ChatClient.java
+++ ChatClient.java
@@ -7,11 +7,16 @@ public class ChatClient {
     String hostName = "fedora";
     try ( 
         Socket chatSocket = new Socket(hostName, portNumber);
-        PrintWriter socketOut = new PrintWriter(chatSocket,portNumber);
-        BufferedReader socketIn = new BufferedReader(new InputSt)
-        BufferedReader stdIn = new BufferedReader(System.in)
-        ) { 
+        PrintWriter socketOut = new PrintWriter(chatSocket.getOutputStream(),true);
+        BufferedReader socketIn = new BufferedReader(new InputStreamReader(chatSocket.getInputStream()));
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        ) {
           System.out.println("Client is connected to server");
+          String userMessage;
+          while ((userMessage = stdIn.readLine()) != null) { 
+            socketOut.println(userMessage);
+          }
+          
         } catch (IOException e) { 
           System.err.println("Couldn't get connection to " + hostName);  
         }
--- ChatServer.java
+++ ChatServer.java
@@ -1,17 +1,39 @@
 import java.net.*;
 import java.io.*;
 
+class openSocket implements Runnable { 
+  @Override 
+  public void run() { 
+    
+  }
+}
+
+public static void acceptClients() { 
+  while (true) { 
+    try { 
+          Socket clientSocket = serverSocket.accept();
+          PrintWriter socketOut = new PrintWriter(clientSocket.getOutputStream(), true);
+          BufferedReader socketIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) { 
+
+        }
+  }
+}
+
 public class ChatServer {
     public static void main(String[] args) {
         int portNumber = 4444;
 
         try (
             ServerSocket serverSocket = new ServerSocket(portNumber);
-            Socket clientSocket = serverSocket.accept();
+            acceptClients();
+            
         ) {
-            // Server logic goes here
             System.out.println("Server is running and waiting for a client to connect.");
-
+            String clientInput; 
+            while ((clientInput = socketIn.readLine()) != null) { 
+              System.out.println(clientInput);
+            }
         } catch (IOException e) {
             System.err.println("Exception caught when trying to listen on port " + portNumber);
             System.err.println(e.getMessage());
